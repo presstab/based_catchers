e4a6ae77e8d262046aed5b0e7692b87979fbf6d0
presstab <presstab1337@gmail.com>
2023-03-08 14:19:34 -0700

    Improve gas efficiency for character data.

--- contracts/characterdata.sol
+++ contracts/characterdata.sol
@@ -1,80 +1,27 @@
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-enum Traits {
-    Strength,
-    Valor,
-    Intelligence,
-    Speed,
-    Magic
-}
-
-uint64 constant MAX = ~uint64(0);
-uint64 constant MASK_STRENGTH = ~(MAX>>8);
-uint64 constant MASK_VALOR = ~(MAX>>16);
-uint64 constant MASK_INTELLIGENCE = ~(MAX>>24);
-uint64 constant MASK_SPEED = ~(MAX>>32);
-uint64 constant MASK_MAGIC = ~(MAX>>40);
-
 contract characterData is Ownable {
 
+    struct Traits {
+        uint8 strength;
+        uint8 valor;
+        uint8 intelligence;
+        uint8 speed;
+        uint8 magic;
+    }
+
     // Nft unique hash (keccak(contract address, id)) => trait data
-    mapping(uint256 => uint64) private mapData;
+    mapping(uint256 => Traits) private mapData;
 
-    function getMask(Traits _trait) private pure returns (uint64) {
-        if (_trait == Traits.Strength) {
-            return MASK_STRENGTH;
-        } else if (_trait == Traits.Valor) {
-            return MASK_VALOR;
-        } else if (_trait == Traits.Intelligence) {
-            return MASK_INTELLIGENCE;
-        } else if (_trait == Traits.Speed) {
-            return MASK_SPEED;
-        } else if (_trait == Traits.Magic) {
-            return MASK_MAGIC;
-        }
-        require(false, "err: invalid trait");
-        return 0;
-    }
-
-    function getShift(Traits _trait) private pure returns (uint8) {
-        if (_trait == Traits.Strength) {
-            return 56;
-        } else if (_trait == Traits.Valor) {
-            return 48;
-        } else if (_trait == Traits.Intelligence) {
-            return 40;
-        } else if (_trait == Traits.Speed) {
-            return 32;
-        } else if (_trait == Traits.Magic) {
-            return 24;
-        }
-        require(false, "err: invalid trait");
-        return 0;
-    }
-
-    function extractBits(uint64 _data, Traits _trait) private pure returns (uint8) {
-        return uint8((_data & getMask(_trait)) >> getShift(_trait));
-    }
-
-    function addData(uint256 _hash, uint8[5] calldata _data) public onlyOwner returns (uint64) {
-        uint64 d = 0;
-        d = d|(uint64(_data[0])<<56);
-        d = d|(uint64(_data[1])<<48);
-        d = d|(uint64(_data[2])<<40);
-        d = d|(uint64(_data[3])<<32);
-        d = d|(uint64(_data[4])<<24);
-        mapData[_hash] = d;
-        return d;
-    }
-
-    function dataSingle(uint256 _hash, Traits _trait) public view returns (uint8) {
-        uint64 d = (mapData[_hash] & getMask(_trait)) >> getShift(_trait);
-        return uint8(d);
+    function addData(uint256 _hash, uint8[5] calldata _data) public onlyOwner {
+        mapData[_hash] = Traits(_data[0], _data[1], _data[2], _data[3], _data[4]);
     }
 
     function data(uint256 _hash) public view returns (uint8, uint8, uint8, uint8, uint8) {
-        uint64 d = mapData[_hash];
-        require(d > 0, "err: no character data");
-        return (extractBits(d, Traits(0)), extractBits(d, Traits(1)), extractBits(d, Traits(2)), extractBits(d, Traits(3)), extractBits(d, Traits(4)));
+        Traits memory d = mapData[_hash];
+        if (d.strength == 0 && d.valor == 0 && d.intelligence == 0 && d.speed == 0 && d.magic == 0) {
+            require(false, "err: no character data");
+        }  
+        return (d.strength, d.valor, d.intelligence, d.speed, d.magic);
     }
 }
